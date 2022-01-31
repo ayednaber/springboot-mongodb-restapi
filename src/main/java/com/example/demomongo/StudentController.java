5fac63117e1867e6188ef82e66d784d6f6ca361e
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-31 09:54:23 -0500

    Added POST, PUT, and DELETE mappings to MongoDB Spring Boot Application

--- src/main/java/com/example/demomongo/StudentController.java
+++ src/main/java/com/example/demomongo/StudentController.java
@@ -1,10 +1,9 @@
 package com.example.demomongo;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -16,5 +15,29 @@ public class StudentController {
     @GetMapping
     public List<Student> fetchAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") String studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") String studentId,
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) Gender gender,
+            @RequestParam(required = false) Address address,
+            @RequestParam(required = false) List<String> favoriteSubjects,
+            @RequestParam(required = false) BigDecimal totalSpentInBooks
+    ) {
+        studentService.updateStudent(studentId, firstName, lastName, email, gender, address, favoriteSubjects, totalSpentInBooks);
     }
 }
--- src/main/java/com/example/demomongo/StudentService.java
+++ src/main/java/com/example/demomongo/StudentService.java
@@ -2,8 +2,13 @@ package com.example.demomongo;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -13,5 +18,64 @@ public class StudentService {
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Student already exists in database");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(String studentId) {
+        boolean studentExists = studentRepository.existsById(studentId);
+        if (!studentExists) {
+            throw new IllegalStateException("Student with " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(String studentId,
+                              String firstName,
+                              String lastName,
+                              String email,
+                              Gender gender,
+                              Address address,
+                              List<String> favoriteSubjects,
+                              BigDecimal totalSpentInBooks) {
+        Student student = studentRepository.findById(studentId).orElseThrow(
+                () -> new IllegalStateException("Student with id " + studentId + " does not exist")
+        );
+
+        if (firstName != null && firstName.length() > 0 && !Objects.equals(student.getFirstName(), firstName)) {
+            student.setFirstName(firstName);
+        }
+
+        if (lastName != null && lastName.length() > 0 && !Objects.equals(student.getLastName(), lastName)) {
+            student.setLastName(lastName);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            student.setEmail(email);
+        }
+
+        if (gender != null && !Objects.equals(student.getGender(), gender)) {
+            student.setGender(gender);
+        }
+
+        student.setAddress(address);
+
+        if (favoriteSubjects != null && favoriteSubjects.size() > 0 && !Objects.equals(student.getFavoriteSubjects(), favoriteSubjects)) {
+            student.setFavoriteSubjects(favoriteSubjects);
+        }
+
+        if (totalSpentInBooks != null && !Objects.equals(student.getTotalSpentInBooks(), totalSpentInBooks)) {
+            student.setTotalSpentInBooks(totalSpentInBooks);
+        }
+
+
+
     }
 }
